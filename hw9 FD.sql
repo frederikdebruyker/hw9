05dc967b92c63caa015c24780d39329b09a5b564
frederikdebruyker <frederik@debruyker.com>
2019-04-07 15:24:14 -0500

    updated comment lines based upon Cam's update of the assignment

--- hw9 FD.sql
+++ hw9 FD.sql
@@ -1,21 +1,20 @@
--- use sakita database
+# use sakita database
 USE sakila;
 
--- 1a.	Display the first and last names of all actors from the table actor.
+# 1a.	Display the first and last names of all actors from the table `actor`.
 SELECT 
-    first_name, last_name
+    first_name 'first name', last_name 'last name'
 FROM
     actor;
 
--- 1b.	Display the first and last name of each actor in a single column in upper case letters. Name the column 
--- 		Actor Name.
+# 1b.	Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`.
 SELECT 
     UPPER(CONCAT(first_name, ' ', last_name)) AS 'Actor Name'
 FROM
     actor;
-       
--- 2a.	You need to find the ID number, first name, and last name of an actor, of whom you know only the first 
--- 		name, "Joe." What is one query would you use to obtain this information?
+
+# 2a.	You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
+# 		What is one query would you use to obtain this information?
 SELECT 
     actor_id 'ID number', first_name 'first name', last_name 'last name'
 FROM
@@ -23,7 +22,7 @@ FROM
 WHERE
     first_name = 'Joe';
 
--- 2b.	Find all actors whose last name contain the letters GEN:
+# 2b.	Find all actors whose last name contain the letters `GEN`:
 SELECT 
     *
 FROM
@@ -31,8 +30,7 @@ FROM
 WHERE
     last_name LIKE '%GEN%';
 
--- 2c.	Find all actors whose last names contain the letters LI. This time, order the rows by last name and 
--- 		first name, in that order:
+# 2c.	Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, in that order:
 SELECT 
     *
 FROM
@@ -41,49 +39,45 @@ WHERE
     last_name LIKE '%LI%'
 ORDER BY last_name , first_name;
 
--- 2d.	Using IN, display the country_id and country columns of the following countries: Afghanistan, 
--- 		Bangladesh, and China:
+# 2d.	Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:
 SELECT 
     country_id, country
 FROM
     country
 WHERE
     country IN ('Afghanistan' , 'Bangladesh', 'China');
-    
--- 3a.	You want to keep a description of each actor. You don't think you will be performing queries on a 
--- 		description, so create a column in the table actor named description and use the data type BLOB (Make 
--- 		sure to research the type BLOB, as the difference between it and VARCHAR are significant).
-ALTER TABLE actor
-ADD COLUMN description BLOB;  -- null after 'last update'
--- SELECT * FROM actor;
 
--- 3b.	Very quickly you realize that entering descriptions for each actor is too much effort. Delete the 
--- 		description column.
+# 3a.	You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a 
+# 		column in the table `actor` named `description` and use the data type `BLOB` (Make sure to research the type `BLOB`, as the 
+# 		difference between it and `VARCHAR` are significant).
+ALTER TABLE actor
+ADD COLUMN description BLOB;  # null after 'last update'
+
+# 3b.	Very quickly you realize that entering descriptions for each actor is too much effort. Delete the `description` column.
 ALTER TABLE actor
 DROP COLUMN description;
--- SELECT * FROM actor;
 
--- 4a.	List the last names of actors, as well as how many actors have that last name.
+# 4a.	List the last names of actors, as well as how many actors have that last name.
 SELECT 
-    last_name, COUNT(*) AS count
+    last_name 'last name', #header change not required per question but aligning with next exercise
+    COUNT(*) AS count
 FROM
     actor
 GROUP BY last_name
-ORDER BY count DESC;
+ORDER BY count DESC; # this is not required per question but useful to validate next question
 
--- 4b.	List last names of actors and the number of actors who have that last name, but only for names that are 
--- 	shared by at least two actors
+# 4b.	List last names of actors and the number of actors who have that last name, but only for names that are shared by at least 
+# 		two actors
 SELECT 
-    last_name,
+    last_name 'last name',
     COUNT(last_name) AS 'number of actors with same last name'
 FROM
     actor
 GROUP BY last_name
 HAVING COUNT(last_name) >= 2;
 
--- 4c.	The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query 
--- 		to fix the record.
--- SELECT * FROM actor WHERE first_name = 'GROUCHO';
+# 4c.	The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. Write a query to fix the 
+# 		record.
 UPDATE actor 
 SET 
     first_name = 'HARPO'
@@ -91,27 +85,27 @@ WHERE
     first_name = 'GROUCHO'
         AND last_name = 'WILLIAMS';
     
--- 4d.	Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name 
--- 		after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
+# 4d.	Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a 
+# 		single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
 UPDATE actor 
 SET 
     first_name = 'GROUCHO'
 WHERE
     first_name = 'HARPO';
 
--- 5a.	You cannot locate the schema of the address table. Which query would you use to re-create it?
+# 5a.	You cannot locate the schema of the `address` table. Which query would you use to re-create it?
 SHOW CREATE TABLE address;
 
--- 6a.	Use JOIN to display the first and last names, as well as the address, of each staff member. Use the 
--- 		tables staff and address: 
--- join showing staff members with an address
+# 6a.	Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and 
+# 		`address`:
+## join showing staff members with an address
 SELECT 
     staff.first_name, staff.last_name, address.address
 FROM
     staff
         INNER JOIN
     address ON staff.address_id = address.address_id;
--- join showing staff members with an address if available
+## join showing staff members with an address if available
 SELECT 
     staff.first_name, staff.last_name, address.address
 FROM
@@ -119,8 +113,7 @@ FROM
         LEFT JOIN
     address ON staff.address_id = address.address_id;
 
--- 6b.	Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff 
--- 		and payment.
+# 6b.	Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`.
 SELECT 
     staff.first_name, staff.last_name, SUM(payment.amount)
 FROM
@@ -131,9 +124,8 @@ WHERE
     DATE(payment_date) BETWEEN '2005-08-01' AND '2005-08-31'
 GROUP BY staff.first_name , staff.last_name;
 
--- 6c.	List each film and the number of actors who are listed for that film. Use tables film_actor and film. 
--- 		Use inner join.
-SELECT 
+# 6c.	List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
+SELECT
     film.title, COUNT(*) AS 'number of actors'
 FROM
     film_actor
@@ -142,7 +134,10 @@ FROM
 GROUP BY film_actor.film_id
 ORDER BY film.title;
 
--- 6d.	How many copies of the film Hunchback Impossible exist in the inventory system?
+# 6d.	How many copies of the film `Hunchback Impossible` exist in the inventory system?
+## Note that depending on the interpretation the result could be very different:
+## - if we go for a binary comparison, the answer is 0
+## - if we go for a non-binary comparison and the default setup, the select is not case sensitive. Result below:
 SELECT 
     COUNT(*) AS 'copies of the film Hunchback Impossible'
 FROM
@@ -153,12 +148,14 @@ WHERE
     film.title = 'Hunchback Impossible'
 GROUP BY inventory.film_id;
 
--- 6e.	Using the tables payment and customer and the JOIN command, list the total paid by each customer. List 
--- 		the customers alphabetically by last name:
-SELECT 
-    customer.last_name'last name',
-    customer.first_name'first name',
-    SUM(payment.amount)'total paid'
+# 6e.	Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically 
+# 		by last name:
+## A left join was considered for this question in case we have customers who were entered into the system but never paid anything. I assumed
+## this was not part of our question.
+SELECT
+    customer.last_name 'last name',
+    customer.first_name 'first name',
+    SUM(payment.amount) 'total paid'
 FROM
     customer
         INNER JOIN
@@ -166,10 +163,12 @@ FROM
 GROUP BY customer.customer_id
 ORDER BY customer.last_name;
 
--- 7a.	The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, 
--- 		films starting with the letters K and Q have also soared in popularity. Use subqueries to display the 
--- 		titles of movies starting with the letters K and Q whose language is English.
-SELECT 
+# 7a.	The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters `K` 
+#		and `Q` have also soared in popularity. Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is 
+#		English.
+## I included 2 options. I believe option 1 is the way to resolve the issue, but if I go to the letter of the question, I added option 2:
+### option 1 with subquery for the language 'English'.
+SELECT
     title
 FROM
     film
@@ -181,10 +180,11 @@ WHERE
             language
         WHERE
             name = 'English');
+### option 2 with subqueries on title and language would include a double select on title
 
--- 7b.	Use subqueries to display all actors who appear in the film Alone Trip.
+# 7b.	Use subqueries to display all actors who appear in the film `Alone Trip`.
 SELECT 
-    actor.last_name, actor.first_name
+    actor.last_name 'last name', actor.first_name 'first name'
 FROM
     film_actor
         INNER JOIN
@@ -198,8 +198,9 @@ WHERE
             title = 'Alone Trip')
 ORDER BY actor.last_name , actor.first_name;
 
--- 7c.	You want to run an email marketing campaign in Canada, for which you will need the names and email 
--- 		addresses of all Canadian customers. Use joins to retrieve this information.
+# 7c.	You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use 
+#		joins to retrieve this information.
+## joins
 SELECT 
     customer.first_name 'first name',
     customer.last_name 'last name',
@@ -215,7 +216,7 @@ FROM
 WHERE
     country.country = 'Canada'
 ORDER BY customer.last_name , customer.first_name;
--- same outcome through subqueries
+## same outcome through subqueries
 SELECT 
     customer.first_name 'first name',
     customer.last_name 'last name',
@@ -238,8 +239,8 @@ WHERE
                     country = 'Canada'))
 ORDER BY customer.last_name , customer.first_name;
 
--- 7d.	Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
--- 		Identify all movies categorized as family films.
+# 7d.	Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized 
+#		as family films.
 SELECT 
     film.title
 FROM
@@ -255,7 +256,7 @@ WHERE
             name = 'Family')
 ORDER BY film.title;
 
--- 7e.	Display the most frequently rented movies in descending order.
+# 7e.	Display the most frequently rented movies in descending order.
 SELECT 
     film.title, COUNT(rental_id) AS 'most frequently rented movies'
 FROM
@@ -267,7 +268,7 @@ FROM
 GROUP BY film.title
 ORDER BY COUNT(rental_id) DESC;
 
--- 7f.	Write a query to display how much business, in dollars, each store brought in.
+# 7f.	Write a query to display how much business, in dollars, each store brought in.
 SELECT 
     store.store_id 'store ID',
     SUM(payment.amount) 'business, in dollars'
@@ -279,7 +280,7 @@ FROM
     payment ON staff.staff_id = payment.staff_id
 GROUP BY store.store_id;
 
--- 7g.	Write a query to display for each store its store ID, city, and country.
+# 7g.	Write a query to display for each store its store ID, city, and country.
 SELECT 
     store.store_id 'store ID',
     city.city 'city',
@@ -293,9 +294,7 @@ FROM
         INNER JOIN
     country ON city.country_id = country.country_id;
 
--- 7h.	List the top five genres in gross revenue in descending order. (Hint: you may need to use the following 
--- 		tables: category, film_category, inventory, payment, and rental.)
--- > to do: sort does not work
+# 7h.	List the top five genres in gross revenue in descending order.
 SELECT 
     category.name 'top five genres',
     SUM(payment.amount) 'gross revenue'
@@ -313,10 +312,8 @@ GROUP BY category.name
 ORDER BY sum(payment.amount) DESC
 LIMIT 5;
 
--- 8a.	In your new role as an executive, you would like to have an easy way of viewing the Top five genres by 
--- 		gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you 
--- 		can substitute another query to create a view.
--- > to do: update after finalizing 7h
+# 8a.	In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from 
+#		the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
 CREATE VIEW 8a_v AS
     SELECT 
         category.name 'top five genres',
@@ -335,12 +332,12 @@ CREATE VIEW 8a_v AS
     ORDER BY SUM(payment.amount) DESC
     LIMIT 5;
 
--- 8b.	How would you display the view that you created in 8a?
+# 8b.	How would you display the view that you created in 8a?
 SELECT 
     *
 FROM
     8a_v;
 
--- 8c.	You find that you no longer need the view top_five_genres. Write a query to delete it.
+# 8c.	You find that you no longer need the view `top_five_genres`. Write a query to delete it.
 DROP VIEW IF EXISTS 
 	8a_v;
